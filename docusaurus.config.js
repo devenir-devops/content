438e4735ef44dda0904d16b8fbe1df9aaadb7558
Thibault Cordier <thibault.cordier@gmail.com>
2022-10-30 09:35:35 +0100

    Change edit Urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/devenir-devops/content/tree/main/docs",
           remarkPlugins: [oembed],
         },
         blog: {
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/devenir-devops/content/tree/main/blog",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
